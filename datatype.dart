737bc04812a54868e1b7448aaabb4d21959b0f1a
Tsigabuwalker <tsigabu.seyoum-ug@aau.edu.et>
2025-02-16 10:58:41 +0300

    fist commit

---
+++ datatype.dart
@@ -0,0 +1,53 @@
+// Data types 
+// Numbers, Strings, Booleans, Lists, Maps, Dynamic
+//Strings
+void main() {
+  String fistName = "john";
+  print("String: $fistName");
+  //Integers
+  int age = 40;
+  print("Integer: $age");
+  //DOuble 
+double balance = 870000;
+print("Double: $balance");
+
+
+//List
+var myList = [1000,3990,8900,5690];
+print(myList);
+print(myList[1]);
+
+//Change an item
+myList[0] = 80000000;
+print(myList[0]);
+
+
+
+
+//Create an Empty List
+var emptyList = [];
+print(emptyList);
+
+
+
+//Add multiple to empty List
+
+
+
+
+//Insert at specific position (position, item)
+
+
+//Insertmany
+
+
+
+//Remove from List
+
+
+
+
+
+
+
+}
---
+++ first.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('Hello Tsigbau Software Engineer');
+  var name = "Tsigabu Information Systems graduate";
+  print (name);
+  dynamic variable = "Hello Softwware Enegineers!";
+  print(variable);
+  variable = 2000;
+  print(variable);
+  var x ;
+   print(x);
+   x = "Hello Everybody";
+   print(x);
+
+}
