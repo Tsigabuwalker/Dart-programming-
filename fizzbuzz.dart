862cbad0b1808fbb3b5e2ab03e6f9c6e6ca08670
tsigabuwalker <Seyoumwalker@gmail.com>
2025-02-18 11:10:40 +0300

    added some logic for fizzbuzz

--- fizzbuzz.dart
+++ fizzbuzz.dart
@@ -1,11 +1,24 @@
 void main(){
   var num = 1;
   while(num <= 100){
-    print(num);
+    if (num % 5 == 0 && num % 3 == 0){
+      print("$num. FizzBuzz!");
+    }
+    else if(num % 3 == 0){
+      print("$num. Fizz");
+
+    }
+    else if(num % 5 == 0){
+      print("$num. Buzz!");
+    }
+    else{
+      print("$num.");
+    }
+
 
     //increment the counter;
     num++;
-    
+
 
   }
   
